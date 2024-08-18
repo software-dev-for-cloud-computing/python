a426e3d4bfb4b855bf6f17ed6456e3666c0d24ea
ericbernet <eb076@hdm-stuttgart.de>
2024-08-18 22:36:34 +0200

    fix attempt 3

--- app/services/pdf_reader_service.py
+++ app/services/pdf_reader_service.py
@@ -9,23 +9,24 @@ from app.utils.logger import Logger
 
 logger = Logger('Logger')
 
-
 class PDFReaderService(PDFReader):
     @staticmethod
     @logger.log_decorator(level="debug", message="Reading in PDF")
     async def read_pdf(file):
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            temp_filename = temp_file.name
-            try:
-                async with AIOFile(temp_filename, 'wb') as afp:
-                    await afp.write(file)
-                    await afp.fsync()
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_filename = temp_file.name
+        try:
+            async with AIOFile(temp_filename, 'wb') as afp:
+                await afp.write(file)
+                await afp.fsync()
 
-                loader = PyMuPDFLoader(file_path=temp_filename)
-                data = loader.load()
+            # Lade die Datei in den Speicher
+            loader = PyMuPDFLoader(file_path=temp_filename)
+            data = loader.load()
 
-            finally:
-                time.sleep(0.1)  # Optional: Verzögerung für Sicherheit
-                os.unlink(temp_filename)
+        finally:
+            # Warten, bis die Datei nicht mehr verwendet wird
+            time.sleep(0.1)
+            os.unlink(temp_filename)
 
         return data
