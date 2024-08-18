918170853c68071bf99cdeb58b0848cbf799250a
ericbernet <eb076@hdm-stuttgart.de>
2024-08-18 22:27:46 +0200

    trying to fix file reading problem with windows

--- app/services/pdf_reader_service.py
+++ app/services/pdf_reader_service.py
@@ -14,10 +14,12 @@ class PDFReaderService(PDFReader):
     @logger.log_decorator(level="debug", message="Reading in PDF")
     async def read_pdf(file):
         temp_file = tempfile.NamedTemporaryFile(delete=False)
-        async with AIOFile(temp_file.name, 'wb') as afp:
-            await afp.write(file)
-            await afp.fsync()
-        loader = PyMuPDFLoader(file_path=temp_file.name)
-        data = loader.load()
-        os.unlink(temp_file.name)
+        try:
+            async with AIOFile(temp_file.name, 'wb') as afp:
+                await afp.write(file)
+                await afp.fsync()
+            loader = PyMuPDFLoader(file_path=temp_file.name)
+            data = loader.load()
+        finally:
+            os.unlink(temp_file.name)
         return data
